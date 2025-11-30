f8ca1dfc6ecea02ff523e4116d0b9066544fd195
Pau Erola <pauerola@gmail.com>
2025-11-30 23:07:44 +0000

    add stats_lib

---
+++ stats_lib.py
@@ -0,0 +1,42 @@
+"""
+A simple library for calculating basic statistics.
+
+© 2025 Pau Erola, University of Bristol
+"""
+
+from typing import List, Union
+
+
+def mean(data: List[Union[int, float]]) -> float:
+    """
+    Calculate the mean (average) of a list of numbers.
+    
+    Args:
+        data: A list of numbers
+        
+    Returns:
+        The mean as a float
+        
+    Example:
+        >>> mean([1, 2, 3, 4, 5])
+        3.0
+    """
+    return sum(data) / len(data)
+
+
+def variance(data: List[Union[int, float]]) -> float:
+    """
+    Calculate the variance of a list of numbers.
+    
+    Args:
+        data: A list of numbers
+        
+    Returns:
+        The variance as a float
+        
+    Example:
+        >>> variance([1, 2, 3, 4, 5])
+        2.0
+    """
+    avg = mean(data)
+    return sum((x - avg) ** 2 for x in data) / len(data)
